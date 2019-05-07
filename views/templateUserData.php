4ebdd011a719840874d72c3b5bf7551c88749b90
Vincent_Boutot <VincentBoutot@live.ca>
2019-05-07 09:47:31 -0400

    template pour les post -- a modifier

---
+++ views/templateUserData.php
@@ -0,0 +1,5 @@
+
+<input type="hidden" name="ip" value="<?=$_POST['ip']?>"/>
+<input type="hidden" name="user" value="<?=$_POST['user']?>"/>
+<input type="hidden" name="connection" value="<?=json_encode($conn)?>"/>
+<input type="hidden" name="password" value="<?=$_POST['password']?>"/>
